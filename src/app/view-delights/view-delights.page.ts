ec418972ad3c2801daa1dbb20cdeff6987a3ad49
LeelaShankar <lsankar@mindboard.com>
2019-10-01 00:43:19 +0530

    view my delights

--- src/app/view-delights/view-delights.page.ts
+++ src/app/view-delights/view-delights.page.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
+import { NavParams, NavController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-view-delights',
   templateUrl: './view-delights.page.html',
   styleUrls: ['./view-delights.page.scss'],
 })
-export class ViewDelightsPage implements OnInit {
 
-  constructor(public navParams: NavParams) {
+export class ViewDelightsPage implements OnInit {
+  clusters: Array<any> = []
+  groupNames: Array<any> = [];
+  constructor(public navParams: NavParams, public navCtrl: NavController, public modalCtrl: ModalController) {
 
   }
 
   ngOnInit() {
     console.log('navParams', this.navParams)
+    let data = this.navParams.data.data
+    this.clusters = data.activetransactions;
+    this.groupNames = data.groupnames;
   }
 
+  logout() {
+    localStorage.clear();
+    this.navCtrl.navigateRoot('')
+  }
+
+
+  getClusterName(cluster) {
+    let groupObj = this.groupNames.filter(x => x.groupid == cluster.groupid);
+    return groupObj[0] && groupObj[0].groupname;
+  }
+
+  close() {
+    this.modalCtrl.dismiss()
+  }
 }
